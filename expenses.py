26e9cce635273dd5eb3e182dc1b563cb4403eba7
Zuluzeed0077 <zuluyusufolatunji@gmail.com>
2026-09-08 21:39:43 +0000

    start

---
+++ expenses.py
@@ -0,0 +1,14 @@
+amount = 500
+category = "Food"
+description = "Snacks"
+
+expenses = {   
+"amount": amount,
+"category": category,
+"description": description
+}
+
+expenses_list = [expenses]
+
+print(expenses_list)
+print(category)
