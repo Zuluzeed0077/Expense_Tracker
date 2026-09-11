769e2997d6b3e049bc2adf669a5dac84cf4a70f2
yzulu <zuluyusufoltunji@example.com>
2026-09-11 15:00:20 +0100

    update

--- expenses.py
+++ expenses.py
@@ -8,17 +8,21 @@ expenses = {
 "description": description
 }
 
-amount = float(input("Enter the amount spent: "))
-category = input("Enter the category of the expense: ")
-description = input("Enter a description for the expense: ")
-
-expenses2 = {
-    "amount": amount,
-     "category": category,
-     "description": description
-}
-
-expenses_list = [expenses, expenses2]
+move_on = input("Do you want to add another expense? (yes/no): ")
+expenses_list = [expenses]
 
 print(expenses_list[0])
-print(expenses_list[1])
+if move_on == "yes":
+    amount = float(input("Enter the amount spent: "))
+    category = input("Enter the category of the expense: ")
+    description = input("Enter a description for the expense: ")
+
+    expenses2 = {
+        "amount": amount,
+        "category": category,
+        "description": description
+    }
+    expenses_list.append(expenses2)
+
+print(expenses_list)
+
