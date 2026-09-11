bbe5e78a863ca68744c307f3dee7a8a7ae130016
Zuluzeed0077 <zuluyusufolatunji@gmail.com>
2026-09-11 10:25:52 +0100

    Refactor expense tracking with user input
    Refactor expenses to take user input for amount, category, and description. Update expenses_list to include two expense entries.

--- expenses.py
+++ expenses.py
@@ -1,6 +1,6 @@
-amount = 500
-category = "Food"
-description = "Snacks"
+amount = float(input("Enter the amount spent: "))
+category = input("Enter the category of the expense: ")
+description = input("Enter a description for the expense: ")
 
 expenses = {   
 "amount": amount,
@@ -8,7 +8,17 @@ expenses = {
 "description": description
 }
 
-expenses_list = [expenses]
+amount = float(input("Enter the amount spent: "))
+category = input("Enter the category of the expense: ")
+description = input("Enter a description for the expense: ")
 
-print(expenses_list)
-print(category)
+expenses2 = {
+    "amount": amount,
+     "category": category,
+     "description": description
+}
+
+expenses_list = [expenses, expenses2]
+
+print(expenses_list[0])
+print(expenses_list[1])
