5ce8b697f032123cbb55eb41ee1bdd8d970c1c64
Zuluzeed0077 <zuluyusufolatunji@gmail.com>
2026-09-15 08:41:48 +0100

    Change expense addition to use of while loop

--- expenses.py
+++ expenses.py
@@ -12,17 +12,17 @@ move_on = input("Do you want to add another expense? (yes/no): ")
 expenses_list = [expenses]
 
 print(expenses_list[0])
-if move_on == "yes":
+while move_on == "yes":
     amount = float(input("Enter the amount spent: "))
     category = input("Enter the category of the expense: ")
     description = input("Enter a description for the expense: ")
 
-    expenses2 = {
+    new_expenses = {
         "amount": amount,
         "category": category,
         "description": description
     }
-    expenses_list.append(expenses2)
+    move_on = input("Do you want to add another expense? (yes/no): ")
+    expenses_list.append(new_expenses   )
 
 print(expenses_list)
-
